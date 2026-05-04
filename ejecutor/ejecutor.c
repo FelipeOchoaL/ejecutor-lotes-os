bfd0dc23ba9d077362531d4d34186baef011fb4e
FelipeOchoaL <pipeochoa22@gmail.com>
2026-05-04 14:45:16 -0500

    base-commit

---
+++ ejecutor/ejecutor.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Iniciando ejecutor...\n");
+    // TODO: Implementar lógica de ejecución de lotes (ejecutor)
+    return 0;
+}
---
+++ gesprog/gesprog.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Iniciando gesprog...\n");
+    // TODO: Implementar lógica de gestión de programas (gesprog)
+    return 0;
+}
